8745a23ecfea94791ad74fabe025aa588892b731
hunk2109 <hectojo_2109@hotmail.com>
2023-01-27 22:55:39 -0400

    1

---
+++ deliberyfast/android/app/src/main/kotlin/com/example/curiosity/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.delibery.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ deliberyfast/android/app/src/main/kotlin/com/example/deliberyfast/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.deliberyfast
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
